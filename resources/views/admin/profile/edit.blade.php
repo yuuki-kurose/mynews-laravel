c3add848815be131ab8d0f2668d065bcf974b5f1
yuuki-kurose <ec2-user@ip-172-31-37-255.ap-northeast-1.compute.internal>
2023-09-20 07:53:33 +0000

    Add: debuger, profile setting

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -4,8 +4,55 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>データの追加</h2>
+                <h2>データの編集</h2>
+                <form action="{{ route('admin.profile.update') }}" method="post" enctype="multipart/form-data">
+
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" placeholder="氏名を入力してください" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">性別</label>
+                        <div class="col-md-10">
+                            <select name="gender" class="form-control">
+                                <option>性別を選択してください</option>
+                                <option value="男性">男性</option>
+                                <option value="女性">女性</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">趣味</label>
+                        <div class="col-md-10">
+                            <select name="hobby" class="form-control">
+                                <option>趣味を選択してください</option>
+                                <option value="サッカー">サッカー</option>
+                                <option value="バスケ">バスケ</option>
+                                <option value="読書">読書</option>
+                                <option value="食事">食事</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">自己紹介</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" placeholder="自己紹介を入力してください" value="{{ old('introduction') }}"></textarea>
+                        </div>
+                    </div>
+                    @csrf
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form>
             </div>
         </div>
     </div>
 @endsection
+            
